6333b23374e714a1d43e11622bab72a75057d0e7
RishiJ12 <rishijagtap12@gmail.com>
2022-03-10 00:39:08 -0800

    sorts a list

--- sort.py
+++ sort.py
@@ -1,29 +1,28 @@
 def sort_list(myList):
     n = len(myList)
     i = 0
-    valid = True
+    result = True
+    for ele in myList:
+        if not isinstance(ele, type(myList[0])):
+            result = False
+            break
 
-    
     while i < n:
         j = 0
         while j < (n - i - 1):
-
-            if myList[j] > myList[j+1]:
-                if isinstance(myList[j], int) and isinstance(myList[j+1], int):
+            if result:
+                if myList[j] > myList[j+1]:
                     myList[j], myList[j + 1] = myList[j + 1], myList[j]
-                else:
-                    valid = False
-
+                
             j = j + 1
 
         i = i + 1
 
 
-    if valid:
+    if result:
         return myList
     else:
         print('List is not valid')
-
 
 
 
