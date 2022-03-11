d7ea62f1e0f65187f7af409dd01e6ef46c1a646b
RishiJ12 <rishijagtap12@gmail.com>
2022-03-10 18:34:58 -0800

    timeit.py

--- timeit.py
+++ timeit.py
@@ -3,7 +3,6 @@ import time
 def calculate_time(func):
     def wrapper_func():
         start = time.time()
-        func()
         end = time.time()
         print('Total Time ', str(end - start))
         return func()
