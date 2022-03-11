44fdb1c1298ab05fbff757b78dfaf15848a323ec
RishiJ12 <rishijagtap12@gmail.com>
2022-03-10 20:07:44 -0800

    added tracker.py

--- tracker.py
+++ tracker.py
@@ -1,9 +1,8 @@
 def func_counter(func):
-    counter = 0
-    def wrapper_func(*args):
-        func_counter.counter += 1
-        return func(*args)
-    func_counter.counter = 0
+    def wrapper_func(*args, *kwargs):
+        wrapper_func += 1
+        return func(*args, *kwargs)
+    wrapper_func.counter = 0
     return wrapper_func
 
 
