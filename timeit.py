11c202c6e2de7219b473910fe70a87c53d99013d
RishiJ12 <rishijagtap12@gmail.com>
2022-03-10 18:18:49 -0800

    timeit.py

--- timeit.py
+++ timeit.py
@@ -1,8 +1,14 @@
 import time
 
 def calculate_time(func):
-    func
-    print('Total time ' + str(time.time())
+    @wraps(func)
+    def timeit_wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        total_time = time.time()
+        # first item in the args, ie `args[0]` is `self`
+        print(f' Total time {total_time:.4s}')
+        return result
+    return timeit_wrapper
 
 
 
